729df9fecf6fa01951e5fb4af6211d1fb6b9ca62
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-02-02 13:20:55 +0500

    Added API Call to OpenWeatherMap for weather data

---
+++ weather.js
@@ -0,0 +1,21 @@
+class Weather{
+  constructor(city,state){
+    this.apiKey = '2884558bcf9a71a0a0be2126d92431dc';
+    this.city = city;
+    this.state = state;
+  }
+
+  async getWeather(){
+    const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+
+    const data = await response.json();
+
+    return data;
+  }
+
+  // Change Location
+  changeLocation(city,state){
+    this.city= city;
+    this.state = state;
+  }
+}
